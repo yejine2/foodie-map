a79f8e08053f818fb19dea56122dae1f07ae8177
yejine2 <yejine2@naver.com>
2024-06-17 15:17:42 +0900

    refactor: 지도 마커 분리, Markers 컴포넌트 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 import Map from "@/components/Map";
+import Markers from "@/components/Markers";
+
+import * as stores from "@/data/store_data.json";
 
 export default function Home() {
-  return <Map />;
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeDatas = stores?.DATA;
+
+  return (
+    <>
+      <Map setMap={setMap} />
+      <Markers
+        storeDatas={storeDatas}
+        map={map}
+        setCurrentStore={setCurrentStore}
+      />
+    </>
+  );
 }
