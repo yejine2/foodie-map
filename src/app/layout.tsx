e5a437394d04789c35d01fb2f978d4c8f99f58b6
yejine2 <yejine2@naver.com>
2024-07-15 20:33:40 +0900

    feat: app router로 마이그레이션 - global layout 및 메인페이지 변경

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import { NextLayout, NextProvider } from "./providers";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "푸디맵 | 맛집 지도",
+  description: "푸디맵으로 쉽게 찾는 나의 맛집",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>
+        <NextProvider>
+          <NextLayout>{children}</NextLayout>
+        </NextProvider>
+      </body>
+    </html>
+  );
+}
